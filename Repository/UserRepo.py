dd9eb2686d186750f99cd98bdde24b7785058105
BUNROEUN SEREYSARAKROETTH <bunroeun.sereysarakroetth.2822@rupp.edu.kh>
2026-03-18 14:23:00 +0700

    CRUD of user

--- Repository/UserRepo.py
+++ Repository/UserRepo.py
@@ -2,6 +2,8 @@ from Models.Auth.UserModel import User as UserModel
 from sqlalchemy.orm import Session
 from Schemas.UserSchemas import UserCreate as UserCreateSchema
 
+
+# create user
 def create_user(db: Session, user: UserCreateSchema):
     db_user = UserModel(
         username=user.username,
@@ -13,10 +15,19 @@ def create_user(db: Session, user: UserCreateSchema):
     db.refresh(db_user)
     return db_user
 
+# get all users
 def get_user_all(db: Session):
     return db.query(UserModel).all()
 
+# get user by id
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(UserModel).filter(UserModel.id == user_id).first()
 
-
-
-
+# delete user by id
+def delete_user_by_id(db: Session, user_id: int):
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
--- Routes/AuthRoutes.py
+++ Routes/AuthRoutes.py
@@ -1,11 +1,38 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from Config.DatabaseConfig import get_db
-from Repository.UserRepo import get_user_all
+from Repository.UserRepo import get_user_all, create_user, get_user_by_id, delete_user_by_id
+from Schemas.UserSchemas import UserCreate as UserCreateSchema
+from Models.Auth.UserModel import User as UserModel
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.get("/user")
 async def get_user(db: Session = Depends(get_db)):
     users = get_user_all(db)
-    return {"message": "User endpoint", "db_status": "Connected", "db_url": str(db.bind.url), "users": users}
+    return {"message": "User endpoint", "db_status": "Connected", "users": users}
+
+@router.post("/user")
+async def create_user(db: Session = Depends(get_db),user : UserCreateSchema = Depends()):
+    new_user = create_user(db, user)
+    return {"message": "User created successfully", "user": new_user}
+    
+#  get user by id
+@router.get("/user/{user_id}")
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = get_user_by_id(db, user_id)
+    if user:
+        return {"message": "User found", "user": user}
+    else:
+        return {"message": "User not found"}
+    
+
+# delete user by id
+@router.delete("/user/{user_id}")
+async def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    success = delete_user_by_id(db, user_id)
+    
+    if success:
+        return {"message": "User deleted successfully"}
+    else:
+        return {"message": "User not found"}
